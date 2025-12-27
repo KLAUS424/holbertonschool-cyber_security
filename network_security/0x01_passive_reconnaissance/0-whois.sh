b382f772a5df1d401cab001a14438084dfa3b94a
witcher <witcher@witcher.witcher>
2025-12-27 03:24:23 -0500

    passiv recon w whois

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -F': ' '/^(Registrant|Admin|Tech)/ && !/ID:/ {gsub(/^[ \t]+|[ \t]+$/, "", $2); if($1 ~ /Ext:$/) print $1","; else if($1 ~ /Street$/) print $1","($2?$2" ":" "); else print $1","($2?$2:" ")}' > "${1}.csv"
+whois "$1" | awk -F': ' '/^(Registrant|Admin|Tech)/ && !/ID:/ {gsub(/^[ \t]+|[ \t]+$/, "", $2); if($1 ~ /Ext:$/) print $1", "; else if($1 ~ /Street$/) print $1", "$2; else print $1", "$2}' > "${1}.csv"
